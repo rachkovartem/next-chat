6b917d1ac65674225ed87b89486a8eaa97bbb1f1
rachkovartem <ateee@yandex.ru>
2022-03-21 17:39:42 +0300

    fix: env url

--- hooks/useApi.tsx
+++ hooks/useApi.tsx
@@ -1,6 +1,7 @@
 import axios from "axios";
-const url = process.env.NEXT_PUBLIC_SERVER_URL || process.env.SERVER_URL;
-console.log(url)
+const url = process.env.NEXT_PUBLIC_SERVER_URL ? process.env.NEXT_PUBLIC_SERVER_URL : process.env.SERVER_URL;
+console.log('process.env.NEXT_PUBLIC_SERVER_URL', process.env.NEXT_PUBLIC_SERVER_URL)
+console.log('process.env.SERVER_URL', process.env.SERVER_URL)
 const api = axios.create({
   baseURL: url,
   withCredentials: true,
